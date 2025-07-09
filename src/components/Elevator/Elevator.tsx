fdb06dc84da58a2bfd84a73249c0350e47454b3d
Tomasz <prusak@hotmail.com>
2025-07-09 22:44:53 +0100

    add elevator cabin

--- src/components/Elevator/Elevator.tsx
+++ src/components/Elevator/Elevator.tsx
@@ -1,17 +1,31 @@
-import { Children } from 'react';
-
+import { Controller, Scene } from 'react-scrollmagic';
 function Elevator({ children }) {
-	const elevatorTunel = 'src/assets/images/main-page/elevator/repElevator.png';
-
 	return (
 		<section className=" relative ">
+			<div className="z-15 right-[72px] top-[-145px] absolute">
+				<Controller>
+					<Scene
+						duration={0}
+						pin={true}
+						enabled={true}>
+						<div className=" w-[186px] h-[161px] bg-[url(assets/images/main-page/elevator/anCabin.png)]"></div>
+					</Scene>
+				</Controller>
+			</div>
 			<div
-				className="bg-[url(assets/images/main-page/elevator/repElevator.png)] w-[228px] absolute min-h-full 
-  bg-repeat-y right-12 top-0 z-10"></div>
-
+				className="w-[228px] absolute min-h-full 
+  bg-repeat-y right-12 top-0 z-10 bg-[url(assets/images/main-page/elevator/repElevator.png)]"></div>
 			{children}
 		</section>
 	);
 }
-
 export default Elevator;
+
+//:TODO implement later for all images
+{
+	/* <div
+  style={{'--image-url': `url(${fetchedUrl})`}} 
+  className='bg-[image:var(--image-url)]'>
+    <!-- ... -->
+</div> */
+}
