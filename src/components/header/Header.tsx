046f33207b5612b78fb97313e6f9ed1ab8fda65e
Tomasz <prusak@hotmail.com>
2025-07-21 19:11:11 +0100

    set top menu with message header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import type { HeaderProps } from './HeaderProps';
+import type { HeaderProps, MenuLinksProps } from './HeaderProps';
+import parse from 'html-react-parser';
 
 function Header() {
-	const responsiveMac = 'src/assets/images/main-page/header/sluglogo2.png';
+	const logo = 'src/assets/images/main-page/header/sluglogo2.png';
 	const path = 'http://localhost:8080/api/nav';
 	const [navigationData, setNavigationData] = useState([]);
 	useEffect(() => {
@@ -21,27 +22,91 @@ function Header() {
 
 	return (
 		<div className="grid grid-cols-4 gap-2 py-20">
-			<nav className="col-span-4 col-start-2 col-end-4">
-				<div className="flex justify-between pb-2">
-					<img
-						className="w-[232px] h-[44px]  "
-						src={responsiveMac}
-						alt=""
-					/>
-					<ul className="self-end flex ">
-						{navigationData.map((data: HeaderProps) => {
-							return (
-								<li className="p-3 transition ease-in-out duration-500 hover:text-sluggreen-200 hover:text-shadow-lg text-white">
-									<Link to={data.link}>{data.text}</Link>
-								</li>
-							);
-						})}
-					</ul>
-				</div>
-				<hr className="border-white " />
-			</nav>
+			{navigationData.map((nav: HeaderProps) => {
+				return (
+					<Fragment>
+						<nav className="col-span-4 col-start-2 col-end-4">
+							<div className="flex justify-between pb-2">
+								<Link to={nav.homeLink}>
+									<img
+										className="w-[232px] h-[44px]"
+										src={logo}
+									/>
+								</Link>
+								<ul className="self-end flex ">
+									{nav.menuLinks.map((menu: MenuLinksProps) => {
+										return (
+											<li className="p-3 transition ease-in-out duration-500 hover:text-sluggreen-200 hover:text-shadow-lg text-white">
+												<Link to={menu.link}>{menu.text}</Link>
+											</li>
+										);
+									})}
+								</ul>
+							</div>
+							<hr className="border-white " />
+						</nav>
+						<div className="col-span-4 col-start-2 col-end-2 z-20">
+							<h1 className="text-white">
+								{nav.menuHeader ? parse(nav.menuHeader) : undefined}
+							</h1>
+						</div>
+					</Fragment>
+				);
+			})}
 		</div>
 	);
 }
 
 export default Header;
+
+// import { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import type { HeaderProps, MenuLinksProps } from './HeaderProps';
+
+// function Header() {
+// 	const logo = 'src/assets/images/main-page/header/sluglogo2.png';
+// 	const path = 'http://localhost:8080/api/nav';
+// 	const [navigationData, setNavigationData] = useState<HeaderProps>({
+// 		menuHeader: '',
+// 		homeLink: '',
+// 		menuLinks: [],
+// 	});
+// 	useEffect(() => {
+// 		async function fetchData() {
+// 			const responne = await fetch(path);
+// 			try {
+// 				const resData = await responne.json();
+// 				setNavigationData(resData.data);
+// 			} catch (error) {
+// 				console.log('Data not loaded', error);
+// 			}
+// 		}
+// 		fetchData();
+// 	}, []);
+
+// 	return (
+// 		<div className="grid grid-cols-4 gap-2 py-20">
+// 			<nav className="col-span-4 col-start-2 col-end-4">
+// 				<div className="flex justify-between pb-2">
+// 					<img
+// 						className="w-[232px] h-[44px]"
+// 						src={logo}
+// 						alt=""
+// 					/>
+// 					<ul className="self-end flex ">
+// 						{navigationData ? navigationData?.menuLinks.map((menu: MenuLinksProps) => {
+// 							return (
+// 								<li className="p-3 transition ease-in-out duration-500 hover:text-sluggreen-200 hover:text-shadow-lg text-white">
+// 									<Link to={menu.link}>{menu.text}</Link>
+// 								</li>
+// 							);
+// 						}): null}
+// 					</ul>
+// 				</div>
+// 				<hr className="border-white " />
+// 			</nav>
+// 		</div>
+// 	);
+// }
+
+// export default Header;
--- src/components/header/HeaderProps.ts
+++ src/components/header/HeaderProps.ts
@@ -1,4 +1,10 @@
 export interface HeaderProps {
+	menuHeader: string;
+	homeLink: string;
+	menuLinks: MenuLinksProps[];
+}
+
+export interface MenuLinksProps {
 	text: string;
 	link: string;
 }
